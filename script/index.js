5517c3499d021d20d74bfb16349b868ab00e011e
Masum75-max <almasumabdullah414@gmail.com>
2026-03-08 00:37:30 +0600

    added few things

--- script/index.js
+++ script/index.js
@@ -11,21 +11,21 @@ const btnSet=[openbtn,closedBtn];
 
 
 // for login page
-// const login =document.getElementById("login-btn")
+const login =document.getElementById("login-btn")
 
-// login.addEventListener("click",()=>{
+login.addEventListener("click",()=>{
       
-//     const username=document.getElementById("username").value;
-// const password=document.getElementById("password").value
+    const username=document.getElementById("username").value;
+const password=document.getElementById("password").value
 
-//     if((username.trim().toLowerCase()==="admin")  && (password.trim().toLowerCase()==="admin1234")){
-//         window.location.assign("main.html")
-//     }
-//     else{
-//         alert("Wrong Login Credentials")
-//     }
+    if((username.trim().toLowerCase()==="admin")  && (password.trim().toLowerCase()==="admin1234")){
+        window.location.assign("main.html")
+    }
+    else{
+        alert("Wrong Login Credentials")
+    }
     
-// })
+})
 
 
 
